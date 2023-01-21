7c7f1fb989c8de79d382dee64224941af7f741e6
codeclanstudent <ewan.cruickshank91@googlemail.com>
2023-01-21 12:48:44 +0000

    Initialised all front end components and containers

---
+++ client/src/components/FavouritesList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const FavouritesList = () => {
+
+
+
+    return (
+
+        <>
+
+
+        </>
+    )
+}
+
+export default FavouritesList;
---
+++ client/src/containers/MainContainer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const MainContainer = () => {
+
+
+
+    return (
+
+        <div className="main-container">
+            <h1>GCU Dictionary App</h1>
+
+        </div>
+    )
+}
+
+export default MainContainer;
